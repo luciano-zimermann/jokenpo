7c83097651d1ad4f8ab4ea73e7301c3f7e7f19b6
Luciano Zimermann <lucianozn16@gmail.com>
2023-07-23 22:40:13 -0300

    finishing jokenpo game

--- lib/game.dart
+++ lib/game.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Game extends StatefulWidget {
@@ -7,7 +9,60 @@ class Game extends StatefulWidget {
   _GameState createState() => _GameState();
 }
 
+enum ChooseMove {
+  rock,
+  paper,
+  scissors,
+}
+
 class _GameState extends State<Game> {
+  var _appImage = const AssetImage("images/padrao.png");
+  var _message = 'Escolha uma opção abaixo';
+
+  _selectedOption(ChooseMove _userChoise) {
+    var randomNumber = Random().nextInt(3);
+
+    var _appChoise = ChooseMove.values[randomNumber];
+
+    switch (_appChoise) {
+      case ChooseMove.rock:
+        setState(() {
+          _appImage = const AssetImage("images/pedra.png");
+        });
+        break;
+      case ChooseMove.paper:
+        setState(() {
+          _appImage = const AssetImage("images/papel.png");
+        });
+        break;
+      case ChooseMove.scissors:
+        setState(() {
+          _appImage = const AssetImage("images/tesoura.png");
+        });
+        break;
+    }
+
+    // user winner
+    if (_userChoise == ChooseMove.rock && _appChoise == ChooseMove.scissors ||
+        _userChoise == ChooseMove.scissors && _appChoise == ChooseMove.paper ||
+        _userChoise == ChooseMove.paper && _appChoise == ChooseMove.rock) {
+      _message = "Parabéns! Você ganhou! :)";
+    }
+
+    // app winner
+    else if (_appChoise == ChooseMove.rock &&
+            _userChoise == ChooseMove.scissors ||
+        _appChoise == ChooseMove.scissors && _userChoise == ChooseMove.paper ||
+        _appChoise == ChooseMove.paper && _userChoise == ChooseMove.rock) {
+      _message = "Você perdeu! :( ";
+    }
+
+    // draw
+    else {
+      _message = "Deu empate! :P";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,13 +83,13 @@ class _GameState extends State<Game> {
               ),
             ),
           ),
-          Image.asset('images/padrao.png'),
-          const Padding(
-            padding: EdgeInsets.only(top: 32, bottom: 16),
+          Image(image: _appImage),
+          Padding(
+            padding: const EdgeInsets.only(top: 32, bottom: 16),
             child: Text(
-              'Escolha uma opção abaixo',
+              _message,
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
@@ -43,9 +98,18 @@ class _GameState extends State<Game> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Image.asset('images/pedra.png', height: 100),
-              Image.asset('images/papel.png', height: 100),
-              Image.asset('images/tesoura.png', height: 100),
+              GestureDetector(
+                onTap: () => _selectedOption(ChooseMove.rock),
+                child: Image.asset('images/pedra.png', height: 100),
+              ),
+              GestureDetector(
+                onTap: () => _selectedOption(ChooseMove.paper),
+                child: Image.asset('images/papel.png', height: 100),
+              ),
+              GestureDetector(
+                onTap: () => _selectedOption(ChooseMove.scissors),
+                child: Image.asset('images/tesoura.png', height: 100),
+              ),
             ],
           )
         ],
